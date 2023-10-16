3defb34f528c5f966acbb28b40f7f3622a67e96b
yogeshwar1108 <yogeshwarinapanuri@gmail.com>
2023-10-16 19:33:20 +0530

    sort function done fixed errors

--- src/Droupdown.js
+++ src/Droupdown.js
@@ -3,70 +3,33 @@ import React, { useState } from "react";
 const DropdownMenu = ({ list, setList }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
+  const handleSort = (sortFunction) => {
+    const sortedMovies = [...list].sort(sortFunction);
+    setList(sortedMovies);
     setIsOpen(false);
   };
 
-  const sortByNameAsc = () => {
-    const sortedMovies = [...list].sort((a, b) => {
-      return a.name.localeCompare(b.name);
-    });
-
-    setList(sortedMovies);
-    setIsOpen(false)
-
-  };
-
-  const sortByNameDesc = () => {
-    const sortedMovies = [...list].sort((a, b) => {
-      return b.name.localeCompare(a.name);
-    });
-
-    setList(sortedMovies);
-    setIsOpen(false)
-
-  };
-
-  const sortByReleaseDateAsc = () => {
-    const sortedMovies = [...list].sort((a, b) => {
-      return a.year - b.year;
-    });
-
-    setList(sortedMovies);
-    setIsOpen(false)
-
-  };
-
-  const sortByReleaseDateDesc = () => {
-    const sortedMovies = [...list].sort((a, b) => {
-      return b.year - a.year;
-    });
-
-    setList(sortedMovies);
-    setIsOpen(false)
-
-  };
+  const sortByNameAsc = (a, b) => a.name.localeCompare(b.name);
+  const sortByNameDesc = (a, b) => b.name.localeCompare(a.name);
+  const sortByReleaseDateAsc = (a, b) => b.year - a.year;
+  const sortByReleaseDateDesc = (a, b) => a.year - b.year;
 
   return (
     <div className="dropdown">
-      <button className="dropdown-toggle" onClick={handleOpen}>
+      <button className="dropdown-toggle" onClick={() => setIsOpen(!isOpen)}>
         Sort by
       </button>
       <div className="dropdown-menu" style={{ display: isOpen ? "block" : "none" }}>
-        <a className="dropdown-item" href="#" onClick={sortByNameAsc}>
+        <a className="dropdown-item" href="#" onClick={() => handleSort(sortByNameAsc)}>
           Name (A-Z)
         </a>
-        <a className="dropdown-item" href="#" onClick={sortByNameDesc}>
+        <a className="dropdown-item" href="#" onClick={() => handleSort(sortByNameDesc)}>
           Name (Z-A)
         </a>
-        <a className="dropdown-item" href="#" onClick={sortByReleaseDateAsc}>
+        <a className="dropdown-item" href="#" onClick={() => handleSort(sortByReleaseDateAsc)}>
           Release date (new to old)
         </a>
-        <a className="dropdown-item" href="#" onClick={sortByReleaseDateDesc}>
+        <a className="dropdown-item" href="#" onClick={() => handleSort(sortByReleaseDateDesc)}>
           Release date (old to new)
         </a>
       </div>
